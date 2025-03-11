63285efd8ce7a43d223dc166005e3beeec25f504
ndevu3399 <wachiramwas122@gmail.com>
2025-03-11 21:29:50 +0300

    modify srudent.js

---
+++ student-grade.js
@@ -0,0 +1,8 @@
+function studentGadeGenerator() {
+    let marks = parseInt(prompt("Enter student marks (0-100):"));
+    if (marks > 79) return "A"
+    else if (marks >= 60) return "B";
+    else if (marks >= 50) return "C"
+    else if (marks >= 40) return "D"
+    else return "E"
+}
